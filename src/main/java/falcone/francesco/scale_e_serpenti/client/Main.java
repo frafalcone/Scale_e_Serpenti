6f1692b849b44de9db5e58f8e035c148206d2684
Francesco <95873010+frafalcone@users.noreply.github.com>
2023-09-02 18:03:01 +0200

    Creazione dell'artefatto v0.1

---
+++ src/main/java/falcone/francesco/scale_e_serpenti/client/Main.java
@@ -0,0 +1,9 @@
+package falcone.francesco.scale_e_serpenti.client;
+
+import falcone.francesco.scale_e_serpenti.Grafica.MainWindow;
+
+public class Main {
+    public static void main(String[] args) {
+        MainWindow.main(args);
+    }
+}
